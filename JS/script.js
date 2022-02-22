d67c52d72352860a9f2a87dfb6d8d809ddaa7183
Slava <slvsazonov@gmail.com>
2022-02-22 18:51:24 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert("Fuck you");
+
+//const result = confirm("Are you here?");
+
+//console.log(result);
+
+// const answer = prompt("Are you 18?","18");
+// console.log(answer);
+
+// console.log(typeof(answer));
+
+const answer = [];
+
+answer[0]= prompt("What is your name", "");
+answer[1]= prompt("What is your surname", "");
+answer[2]= +prompt("How old are you", "");
+
+document.write(answer);
+
+
+
---
+++ script2.js
@@ -0,0 +1,22 @@
+// "use strcit";
+
+// const category = 'Vodka';
+
+// console.log('https://someurl.com/'+ category + "/" + "Slava");
+
+// Better to use for the view
+
+
+//  "use strcit";
+
+//  const cars = "Bugatty";
+
+//  console.log(`https://someurl.com/${cars}${"and"}${"audi"}${"lamborgini"}`);
+
+
+ "use strcit";
+
+  const cars = "Bugatty";
+
+  console.log(`https://someurl.com/${cars}/5/lalal/and/lol/bbb/sdsd/fefe`);
+
